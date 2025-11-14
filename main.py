0e5d903e9bc3c23a1dc4d6c536ff3d0241bc34d9
dnda <dndamc@gmail.com>
2025-11-14 16:42:13 -0500

    refac extractin data function to log and manage error

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+import json
+
 import requests
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")  
@@ -12,11 +15,25 @@ CIUDADES = "Arequipa,PE"
 
 def extraer_datos(ciudad):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={API_KEY}&units=metric"
-    print(url)
-    r = requests.get(url)
-    print(f"Status Code: {r.status_code}")
-    print(f"Respuesta: {r.text}")
-    return r.json()
+
+    try:
+        r = requests.get(url,timeout=10)
+        r.raise_for_status()
+        data=r.json()
+        
+        timestamp= datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        path=f"data/raw/{ciudad}_{timestamp}.json"
+        os.makedirs("data/raw",exist_ok=True)
+        
+        with open(path,"w") as f:
+            json.dump(data,f,indent=4)
+            
+        return data
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Erro al extraer datos para {ciudad}: {e}")
+        return None
+        
 
 def transformar_datos(json_data):
     return {
